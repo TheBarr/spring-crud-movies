84988989d79b5b38a0831da3b2e09f156679cdd1
bartek <bartex600700@gmail.com>
2023-10-23 01:30:31 +0200

    add CRUD

--- src/main/java/com/movielibrary/springcrud/MovieRepository.java
+++ src/main/java/com/movielibrary/springcrud/MovieRepository.java
@@ -20,4 +20,17 @@ public class MovieRepository {
     public Movie getById(int id) {
         return jdbcTemplatel.queryForObject("SELECT id, name, rating FROM movie where id=?", BeanPropertyRowMapper.newInstance(Movie.class), id);
     }
+
+    public int save(List<Movie> movies) {
+        movies.forEach(movie -> jdbcTemplatel.update("INSERT INTO movie(name, rating) VALUES (?, ?)", movie.getName(), movie.getRating()));
+        return 1;
+    }
+
+    public int update(Movie movie){
+        return jdbcTemplatel.update("UPDATE movie SET name=?, rating=? WHERE id=?", movie.getName(), movie.getRating(), movie.getId());
+    }
+
+    public int delete(int id){
+        return jdbcTemplatel.update("DELETE FROM movie WHERE id=?", id);
+    }
 }
